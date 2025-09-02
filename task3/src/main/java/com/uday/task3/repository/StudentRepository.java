400f214ea49a14e22685f48e1155a870f1ca454b
ChittalaUday <chitalauday@gmail.com>
2025-09-02 13:49:10 +0530

    fix: Rep

--- task3/src/main/java/com/uday/task3/repository/StudentRepository.java
+++ task3/src/main/java/com/uday/task3/repository/StudentRepository.java
@@ -1,8 +1,10 @@
 package com.uday.task3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.uday.task3.model.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
